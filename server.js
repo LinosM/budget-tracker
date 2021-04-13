4e145a373933b6fe0979953622e5852527050a9c
LinosM <Zemnas768@gmail.com>
2021-04-12 22:01:14 -0400

    removed usecreateindex

--- server.js
+++ server.js
@@ -15,10 +15,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
 });
 
 // routes
