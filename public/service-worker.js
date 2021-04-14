b7b53caf103163f145496edd0aa6a67894d54661
LinosM <Zemnas768@gmail.com>
2021-04-13 21:45:21 -0400

    added chart.js to service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,13 +3,15 @@ const DATA_CACHE_NAME = "data-cache-v2";
 
 
 const FILES_TO_CACHE = [
+  '/',
   '/index.html',
   './styles.css',
   'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
   './dist/index.js',
-  './dist/db.js'
+  './dist/db.js',
+  'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
 ];
 
 // Install
